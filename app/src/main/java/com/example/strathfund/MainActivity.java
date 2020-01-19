1c9a7a3f34c33d2b0e8ecf77872aa5d899e4e418
Nicole <nmuswanya@gmail.com>
2020-01-19 20:51:47 +0300

    Register Page

--- app/src/main/java/com/example/strathfund/MainActivity.java
+++ app/src/main/java/com/example/strathfund/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.strathfund;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View v){
+        if (v.getId() == R.id.btnregister){
+            startActivity(new Intent(this, Register.class));
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/strathfund/Register.java
@@ -0,0 +1,13 @@
+package com.example.strathfund;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
