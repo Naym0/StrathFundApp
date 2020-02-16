ed13d7a60d2723fbeeac8b585fbb62c7e9cf12a4
Nicole <nmuswanya@gmail.com>
2020-02-16 19:12:41 +0300

    Login activity added

--- app/src/main/java/com/example/strathfund/MainActivity.java
+++ app/src/main/java/com/example/strathfund/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClick(View v){
         if (v.getId() == R.id.btnregister){
-            startActivity(new Intent(this, Register.class));
+            Intent intent = new Intent(this, Register.class);
+            startActivity(intent);
+        }
+        else if (v.getId() == R.id.btnlogin){
+            startActivity(new Intent(this, Login.class));
         }
     }
 
