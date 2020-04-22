f3cac577133a33dff0cba100a3241e2ef6ca987e
Nicole <nmuswanya@gmail.com>
2020-04-22 11:04:00 +0300

    Dashboard widgets completed

--- app/src/main/java/com/example/strathfund/Fingerprint_auth.java
+++ app/src/main/java/com/example/strathfund/Fingerprint_auth.java
@@ -1,10 +1,33 @@
 package com.example.strathfund;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.widget.TextView;
+
+import java.security.KeyStore;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 
 public class Fingerprint_auth extends AppCompatActivity {
+    private static final String KEY_NAME = "yourKey";
+    private Cipher cipher;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+    private TextView textView;
+    private FingerprintManager.CryptoObject cryptoObject;
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
