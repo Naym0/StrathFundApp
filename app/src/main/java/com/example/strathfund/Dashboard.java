c5beb8f732f9318654828c9b3e4b646f6def0078
Nicole <nmuswanya@gmail.com>
2020-04-27 20:15:06 +0300

    Optimized imports

--- app/src/main/java/com/example/strathfund/Dashboard.java
+++ app/src/main/java/com/example/strathfund/Dashboard.java
@@ -1,17 +1,8 @@
 package com.example.strathfund;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toolbar;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
     private static final String TAG = "Dashboard Activity";
--- app/src/main/java/com/example/strathfund/Register.java
+++ app/src/main/java/com/example/strathfund/Register.java
@@ -1,6 +1,5 @@
 package com.example.strathfund;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,13 +21,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 import java.util.HashMap;
 import java.util.Map;
